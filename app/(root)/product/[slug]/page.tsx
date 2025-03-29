d90aa6619ae88961415ea314ed97f176d23e9421
luthandodidizav2 <didizaluthando197@gmail.com>
2025-03-29 16:17:15 -0700

    second

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getProductBySlug } from '@/lib/actions/productActions';
 import { notFound } from 'next/navigation';
 import React from 'react'
-import {Badge} from '@/components/ui/Badge'
+import {Badge} from '@/components/ui/badge'
 import {Button} from '@/components/ui/button'
 import {Card,CardContent} from '@/components/ui/card'
 import ProductImageCarousel from '@/components/shared/product/product-image';
